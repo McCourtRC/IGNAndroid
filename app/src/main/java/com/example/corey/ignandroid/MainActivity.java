a6d86e1f7776b5c09032e9a47426533426e132a4
McCourtRC <mccourtrc@gmail.com>
2017-05-22 00:38:35 -0700

    Clean Retrofit

--- app/src/main/java/com/example/corey/ignandroid/MainActivity.java
+++ app/src/main/java/com/example/corey/ignandroid/MainActivity.java
@@ -9,34 +9,16 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.example.corey.ignandroid.models.IGNArticlesResponse;
-import com.example.corey.ignandroid.models.IGNData;
+
 import com.example.corey.ignandroid.models.IGNObject;
-import com.example.corey.ignandroid.models.IGNVideosResponse;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import io.reactivex.Observable;
-
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
-
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,66 +34,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.ign_list_view);
-
-        // Gson for custom type adaptation
-        Gson gson = new GsonBuilder()
-                .setDateFormat(DateFormat.LONG)
-                .create();
-
-        // Build Retrofit
-        Retrofit.Builder builder = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create());
-
-        Retrofit retrofit = builder.build();
-
-        // Build IGN Client for API consumption
-        IGNClient ignClient = retrofit.create(IGNClient.class);
-        Observable<IGNVideosResponse> callVideos = ignClient.getVideos(0,0);
-        Observable<IGNArticlesResponse> callArticles = ignClient.getArticles(0,0);
-
-//        callVideos.subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Observer<IGNVideosResponse>() {
-//                    @Override
-//                    public void onSubscribe(@NonNull Disposable d) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onNext(@NonNull IGNVideosResponse response) {
-//                        // Build relevant IGN Object List
-//                        List<IGNData> data = response.getData();
-//                        List<IGNObject> relevantData = new ArrayList<IGNObject>();
-//
-//                        for (int i = 0; i < data.size(); i++) {
-//                            IGNData d = data.get(i);
-//                            String date = d.getMetadata().getPublishDate();
-//                            String title = d.getMetadata().getName();
-//                            String type = "video";
-//                            String imageUrl = d.getThumbnails().get(1).getUrl();
-//                            String url = d.getMetadata().getUrl();
-//                            IGNObject obj = new IGNObject(date, title, type, imageUrl, url);
-//                            relevantData.add(obj);
-//                        }
-//
-//                        IGNAdapter adapter = new IGNAdapter(MainActivity.this, relevantData);
-//                        listView.setAdapter(adapter);
-//
-//                        Log.d(TAG, "Success");
-//                    }
-//
-//                    @Override
-//                    public void onError(@NonNull Throwable e) {
-//                        Log.d(TAG, "Async Error");
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//
-//                    }
-//                });
 
         String url = "http://ign-apis.herokuapp.com/videos";
 

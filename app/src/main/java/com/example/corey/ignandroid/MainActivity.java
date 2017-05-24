9beade6f1d234655955aef96a444b59882468d9d
McCourtRC <mccourtrc@gmail.com>
2017-05-23 23:05:51 -0700

    Articles request

--- app/src/main/java/com/example/corey/ignandroid/MainActivity.java
+++ app/src/main/java/com/example/corey/ignandroid/MainActivity.java
@@ -17,7 +17,10 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,9 +38,50 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.ign_list_view);
 
-        String url = "http://ign-apis.herokuapp.com/videos";
+        String url = "http://ign-apis.herokuapp.com/articles";
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+//        JsonObjectRequest videosRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                Log.d(TAG, response.toString());
+//
+//                try {
+//                    JSONArray data =  response.getJSONArray("data");
+//                    List<IGNObject> relevantData = new ArrayList<>();
+//
+//                    for (int i = 0; i < data.length(); i++) {
+//                        JSONObject object = data.getJSONObject(i);
+//                        JSONObject metadata = object.getJSONObject("metadata");
+//
+//                        String date = metadata.getString("publishDate");
+//                        String title = metadata.getString("name");
+//                        String type = "video";
+//                        JSONArray thumbnails = object.getJSONArray("thumbnails");
+//                        JSONObject thumb = thumbnails.getJSONObject(0);
+//                        String imageUrl = thumb.getString("url");
+//                        String url = metadata.getString("url");
+//                        relevantData.add(new IGNObject(date, title, type, imageUrl, url));
+//                    }
+//
+//                    IGNAdapter adapter = new IGNAdapter(MainActivity.this, relevantData);
+//                    listView.setAdapter(adapter);
+//                }
+//                catch (final JSONException e) {
+//                    Log.d(TAG, e.toString());
+//                }
+//
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError volleyError) {
+//
+//            }
+//        });
+//
+//        RequestManager.getInstance(this).addToRequestQueue(videosRequest);
+
+        JsonObjectRequest articlesRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 Log.d(TAG, response.toString());
@@ -51,12 +95,13 @@ public class MainActivity extends AppCompatActivity {
                         JSONObject metadata = object.getJSONObject("metadata");
 
                         String date = metadata.getString("publishDate");
-                        String title = metadata.getString("name");
-                        String type = "video";
+                        String title = metadata.getString("headline");
+                        String type = "article";
                         JSONArray thumbnails = object.getJSONArray("thumbnails");
                         JSONObject thumb = thumbnails.getJSONObject(0);
                         String imageUrl = thumb.getString("url");
-                        String url = metadata.getString("url");
+                        String slug = metadata.getString("slug");
+                        String url = articleURL(date, slug);
                         relevantData.add(new IGNObject(date, title, type, imageUrl, url));
                     }
 
@@ -76,7 +121,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        RequestManager.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        RequestManager.getInstance(this).addToRequestQueue(articlesRequest);
 
+    }
+
+    // MARK: - Utilities
+    String articleURL(String dateString, String slug) {
+        String baseURL = "http://www.ign.com/articles/";
+        String urlDate = baseURL + dateStringToUrlDate(dateString);
+        String url = urlDate + slug;
+        return url;
+    }
+
+    String dateStringToUrlDate(String dateString) {
+        String year = new String();
+        String month = new String();
+        String day = new String();
+        try {
+            SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+            year = yearFormat.parse(dateString).toString();
+
+            SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+            month = monthFormat.parse(dateString).toString();
+
+            SimpleDateFormat dayFormat = new SimpleDateFormat("dd");
+            day = dayFormat.parse(dateString).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return year + "/" + month + "/" + day + "/";
     }
 }
